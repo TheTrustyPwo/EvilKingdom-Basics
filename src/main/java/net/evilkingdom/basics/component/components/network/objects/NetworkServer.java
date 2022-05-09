32c5efd9d7bde2e5a3e691545e6fd1711c7e7a96
kodirati <me@kodirati.com>
2022-05-09 01:02:43 -0500

    Ugh me tired

--- src/main/java/net/evilkingdom/basics/component/components/network/objects/NetworkServer.java
+++ src/main/java/net/evilkingdom/basics/component/components/network/objects/NetworkServer.java
@@ -110,6 +110,7 @@ public class NetworkServer {
                 final Transmission transmission = new Transmission(transmissionSite, TransmissionType.REQUEST, this.name, "basics", UUID.randomUUID(), "request=online_players");
                 transmission.send().whenComplete((onlinePlayers, onlinePlayerCountThrowable) -> {
                     final ArrayList<UUID> previousOnlinePlayerUUIDs = this.onlinePlayerUUIDs;
+                    Bukkit.getConsoleSender().sendMessage("op - " + onlinePlayers);
                     if (onlinePlayers.equals("response=request_failed")) {
                         this.onlinePlayerUUIDs.clear();
                     } else {
@@ -117,7 +118,7 @@ public class NetworkServer {
                         jsonArray.forEach(jsonElement -> this.onlinePlayerUUIDs.add(UUID.fromString(jsonElement.getAsString())));
                     }
                     Bukkit.getConsoleSender().sendMessage("previous - " + previousOnlinePlayerUUIDs);
-                    Bukkit.getConsoleSender().sendMessage("current - " + previousOnlinePlayerUUIDs);
+                    Bukkit.getConsoleSender().sendMessage("current - " + this.onlinePlayerUUIDs);
                     previousOnlinePlayerUUIDs.stream().filter(uuid -> !this.onlinePlayerUUIDs.contains(uuid)).map(uuid -> Bukkit.getOfflinePlayer(uuid)).forEach(offlinePlayer -> {
                         Bukkit.getConsoleSender().sendMessage("quit - " + offlinePlayer.getName());
                         Bukkit.getOnlinePlayers().stream().filter(onlinePlayer -> LuckPermsUtilities.getPermissionsViaCache(onlinePlayer.getUniqueId()).contains("basics.network.staff")).forEach(onlinePlayer -> this.plugin.getComponentManager().getFileComponent().getConfiguration().getStringList("components.network.staff.connection.messages.quit.external").forEach(string -> onlinePlayer.sendMessage(StringUtilities.colorize(string.replace("%player%", offlinePlayer.getName()).replace("%server%", this.name)))));
