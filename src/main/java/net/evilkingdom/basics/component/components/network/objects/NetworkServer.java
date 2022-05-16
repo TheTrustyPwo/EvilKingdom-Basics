c50f9d6a4b600eb3bfbd97715cd9a9414ad7fef8
kodirati <me@kodirati.com>
2022-05-16 00:34:19 -0500

    Forgot remove dbeuG

--- src/main/java/net/evilkingdom/basics/component/components/network/objects/NetworkServer.java
+++ src/main/java/net/evilkingdom/basics/component/components/network/objects/NetworkServer.java
@@ -121,7 +121,6 @@ public class NetworkServer {
                     case OFFLINE -> preFormattedStatus = "&coffline";
                 }
                 final String formattedStatus = preFormattedStatus;
-                System.out.println(Bukkit.getOnlinePlayers().stream().filter(onlinePlayer -> LuckPermsUtilities.getPermissionsViaCache(onlinePlayer.getUniqueId()).contains("basics.network.staff")).toList());
                 Bukkit.getOnlinePlayers().stream().filter(onlinePlayer -> LuckPermsUtilities.getPermissionsViaCache(onlinePlayer.getUniqueId()).contains("basics.network.staff")).forEach(onlinePlayer -> {
                     this.plugin.getComponentManager().getFileComponent().getConfiguration().getStringList("components.network.staff.server-status.message").forEach(string -> onlinePlayer.sendMessage(StringUtilities.colorize(string.replace("%server%", this.prettifiedName).replace("%status%", formattedStatus))));
                     onlinePlayer.playSound(onlinePlayer.getLocation(), Sound.valueOf(this.plugin.getComponentManager().getFileComponent().getConfiguration().getString("components.network.staff.status-change.sound.sound")), (float) this.plugin.getComponentManager().getFileComponent().getConfiguration().getDouble("components.network.staff.status-change.sound.volume"), (float) this.plugin.getComponentManager().getFileComponent().getConfiguration().getDouble("components.network.staff.status-change.sound.pitch"));
